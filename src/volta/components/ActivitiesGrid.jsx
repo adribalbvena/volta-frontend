7f3d566088d857db14dd9bc403d48c04a18e10cd
adribalbvena <adribalbuena@hotmail.com>
2023-05-18 11:56:00 +0200

    add plan and add trip implemented

--- src/volta/components/ActivitiesGrid.jsx
+++ src/volta/components/ActivitiesGrid.jsx
@@ -1,17 +1,26 @@
-import { getActivities } from '../../helpers/APIrequests';
+import { addTrip, getActivities } from '../../helpers/APIrequests';
 import './ActivitiesGridStyles.css'
 import { ActivityCard } from './ActivityCard';
 import { useState, useEffect } from "react";
+import { format } from 'date-fns'
 
 
 
 export const ActivitiesGrid = ({destination, startDate, endDate}) => {
   const [activities, setActivities] = useState([]);
+  const [tripId, setTripId] = useState(null)
+
   //const [destinations, setDestinations] = useState([]);
 
   const getPlan = async () => {
     const newActivities = await getActivities(destination, startDate, endDate);
     setActivities(newActivities);
+
+    if (activities) {
+      const trip = await addTrip(destination, format(startDate, 'dd/MM/yyyy'), format(endDate, 'dd/MM/yyyy')) // we need to format the dates bc if not, db doesnt recognize it
+      console.log(trip)
+      setTripId(trip.id)
+    }
   };
 
   //  const getDestinations = async() => {
@@ -29,7 +38,7 @@ export const ActivitiesGrid = ({destination, startDate, endDate}) => {
       <h1 className="activities-title">Activities in {destination}</h1>
       <div className="activities-row">
         {activities.map((activities) => (
-          <ActivityCard destination={destination} startDate={startDate} endDate={endDate} key={activities.day} {...activities} />
+          <ActivityCard tripId = {tripId} key={activities.day} {...activities} />
         ))}
       </div>
     </>
--- src/volta/components/ActivityCard.jsx
+++ src/volta/components/ActivityCard.jsx
@@ -1,35 +1,44 @@
-import { format } from 'date-fns'
-import { addPlan, addTrip } from "../../helpers/APIrequests";
+import { addPlan } from "../../helpers/APIrequests";
 import { useAuth } from "../../helpers/auth";
 import "./ActivityCardStyles.css";
 import { MdCheckBox, MdBookmark, MdBookmarkBorder } from 'react-icons/md'
 import { useState } from 'react';
 
-export const ActivityCard = ({destination, startDate, endDate, day, activities }) => {
+export const ActivityCard = ({tripId, day, activities }) => {
   const auth = useAuth()
   const [isSaved, setIsSaved] = useState(false)
 
   const handleOnSave = async() => {
-    const trip = await addTrip(destination, format(startDate, 'dd/MM/yyyy'), format(endDate, 'dd/MM/yyyy')) // we need to format the dates bc if not, db doesnt recognize it
-    console.log(trip)
+    if (tripId != null) {
+      const planData = {
+        day: day,
+        activities: activities,
+      };
+  
+      try {
+        await addPlan(tripId, planData);
+        setIsSaved(true); 
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    }
 
-    const tripId = trip.id
-    
-    const planData = {
-      day: day,
-      activities: activities,
-    };
-
-    console.log(planData)
-    const plan = await addPlan(tripId, planData);
-    console.log(plan)
-  }
+    const getButtonIcon = () => {
+      if (isSaved) {
+        return <MdBookmark className="i-bookmark" />;
+      } else {
+        return <MdBookmarkBorder className="i-bookmark" />;
+      }
+    }
 
   return (
     <>
       <div className="animate__animated animate__fadeIn activity-container">
         {auth.userId && (
-          <MdBookmarkBorder className="i-bookmark" onClick={handleOnSave}/>
+          <div onClick={handleOnSave}>
+            {getButtonIcon()}
+          </div>
         )}
         <h2 className="activity-day">Day {day}</h2>
         <ul className="activity-list">
